eb98867d65c13f1c5b3d127d5fb3a7de7a42d576
shubhhhGT <shubhamgt001@gmail.com>
2023-09-26 18:53:53 +0530

    Adding URL

---
+++ src/baseUrl.js
@@ -0,0 +1,5 @@
+// External API: Blog Posts API (By CodeHelp)
+// Authentication: not required
+// Please search the Api using a browser to understand the data it provides
+
+export const baseUrl = "https://codehelp-apis.vercel.app/api/get-blogs";
