c5c6e0422a3efd1377d84ee118067c00d1c19a40
shubhhhGT <shubhamgt001@gmail.com>
2023-10-01 11:47:17 +0530

    Updated Post Component
    Added Navlinks to make the posts clickable

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext';
 import Spinner from './Spinner';
+import BlogDetails from './BlogDetails';
+import { useLocation } from 'react-router-dom';
 
 const Posts = () => {
 
     const {loading, posts} = useContext(AppContext);
+    const location = useLocation();
+    const len = posts.length <= 2 ? ('[100vh]') : ('full');
 
     return (
-        <div className='w-10/12 max-w-[670px] py-8 flex flex-col items-center gap-y-7 my-16'>
+        <div className={location.pathname === '/' ? 
+        (`w-10/12 max-w-[670px] h-${len} py-8 flex flex-col items-center gap-y-7 my-16`) :
+        (`w-10/12 max-w-[670px] h-${len} py-8 flex flex-col items-center gap-y-7 mb-16`)}>
             {/* In this component I have to either show loading screeen or the posts 
             If loading is true then show the Spinner component elase show the posts*/}
             {
@@ -20,39 +26,7 @@ const Posts = () => {
                         </div>
                     ) :
                     (posts.map( (post) => (
-                        
-                        <div key={post.id} className='w-[300px] sm:w-full text-textColor transition-all ease-in-out duration-700'>
-                            {/* Mapping all posts so that only one post has to be styled and others will be styled accordingly  */}
-                            <p className='font-bold text-[15px] sm:text-lg'>
-                                {post.title}
-                            </p>
-
-                            <p className='sm:text-sm text-xs'>
-                                By <span className='italic mt-1'>
-                                {post.author}
-                                </span> on <span className='underline font-semibold'>{post.category}</span>
-                            </p>
-
-                            <p className='text-sm mt-1'>
-                                Posted on
-                                <span>{post.date}</span>
-                            </p>
-
-                            <p className='text-md text-justify mt-2'>{post.content}</p>
-
-                            <div className='flex gap-x-3 mt-2'>
-                                {
-                                    post.tags.map( (tag, index) => {
-                                        return(
-                                            <span key={index} className='text-blue-700 underline font-bold text-[9px] sm:text-xs'>
-                                                {`#${tag}`}
-                                            </span>
-                                        )
-                                    })
-                                }
-                            </div>
-
-                        </div>
+                        <BlogDetails key={post.id} post={post}/>
                     )))
                 )
             }
@@ -61,3 +35,37 @@ const Posts = () => {
 }
 
 export default Posts
+
+
+{/* <div key={post.id} className='w-[300px] sm:w-full text-textColor transition-all ease-in-out duration-700'>
+{/* Mapping all posts so that only one post has to be styled and others will be styled accordingly  */}
+{/* <p className='font-bold text-[15px] sm:text-lg'>
+    {post.title}
+</p>
+
+<p className='sm:text-sm text-xs'>
+    By <span className='italic mt-1'>
+    {post.author}
+    </span> on <span className='underline font-semibold'>{post.category}</span>
+</p>
+
+<p className='text-sm mt-1'>
+    Posted on
+    <span>{post.date}</span>
+</p>
+
+<p className='text-md text-justify mt-2'>{post.content}</p>
+
+<div className='flex gap-x-3 mt-2'>
+    {
+        post.tags.map( (tag, index) => {
+            return(
+                <span key={index} className='text-blue-700 underline font-bold text-[9px] sm:text-xs'>
+                    {`#${tag}`}
+                </span>
+            )
+        })
+    }
+</div>
+
+</div> */}
