d9e0c6162882856cce46960e41b3a4aa720f3b2f
shubhhhGT <shubhamgt001@gmail.com>
2023-10-01 11:43:43 +0530

    Updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Darkmode from './Darkmode'
 import '../index.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='flex w-full bg-bgColor shadow-header-shadow py-4 fixed top-0 transition-all ease-in-out duration-700 '>
       <header className='mx-auto'>
-        <h1 className='text-2xl sm:text-3xl font-bold tracking-wider uppercase text-textColor'>codehelp blogs</h1>
+      <Link to={'/'}>
+      <h1 className='text-2xl sm:text-3xl font-bold tracking-wider uppercase text-textColor'>codehelp blogs</h1>
+      </Link>
       </header>
 
       {/* we also need to show the toogle for darkmode */}
