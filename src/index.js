4a252e19eac77d5dc3551e268a30c3b509f634cd
shubhhhGT <shubhamgt001@gmail.com>
2023-10-01 11:45:14 +0530

    Updated index.js
    Added Browser router to add routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AppContextProvider from './context/AppContext';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //wrapping the App component inside the context to pass all the data
   //App component is children of the context
-  <AppContextProvider>
-    <App/> 
-  </AppContextProvider>
+  //wrapping all the components inside BrowserRouter, since we are creating routes
+  <BrowserRouter>
+    <AppContextProvider>
+      <App/> 
+    </AppContextProvider>
+  </BrowserRouter>
 );
 
